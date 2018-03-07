dece6ea8ff3966f1a4f5e293d4c45646acf14fb5
gdcp <gdcp@m11.local>
2018-03-07 15:50:04 +0800

    1959

--- ios/ViewController.swift
+++ ios/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        print("666")
         // Dispose of any resources that can be recreated.
     }
 
